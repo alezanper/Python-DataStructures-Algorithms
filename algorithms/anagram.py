f75e502e611c3404285a48205348e5ec048e83b9
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-20 17:09:54 -0500

    [added] anagram algorithm

---
+++ algorithms/anagram.py
@@ -0,0 +1,38 @@
+def isAnagram(a, b):
+
+
+    # Preparation
+    a = a.replace(" ", "")
+    b = b.replace(" ", "")
+    a = a.lower()
+    b = b.lower()
+
+    # edge cases
+    if ((len(a)==0) or (len(b)==0)):
+        return False
+
+    if ((len(a) != len(b))):
+        return False    
+
+    # Auxiliar Variables
+    dictionary = {}
+
+    for letter in a:
+        if letter in dictionary:
+            dictionary[letter] +=1
+        else:
+            dictionary[letter] = 1
+
+    for letter in b:
+        if letter in dictionary:
+            dictionary[letter] -=1
+        else:
+            dictionary[letter] = 1
+
+    for k in dictionary:
+        if dictionary[k] != 0:
+            return False
+    
+    return True
+
+print(isAnagram('dooiig', 'gooiod'))
