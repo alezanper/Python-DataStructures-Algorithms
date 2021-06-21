63e0bf1973de735f9ecf30aed50935dc9d8e9848
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-20 20:54:05 -0500

    [added] string compression

---
+++ algorithms/string_compression.py
@@ -0,0 +1,18 @@
+# Given a string "AAAABBBBCCCCCDDEEEE" returns "A4B4C5D2E4"
+# Case sensitive 
+# "AAB" returs "A2B1"
+
+def string_compression(text):
+    dictionary = {}
+    compress = ""
+    for letter in text:
+        if letter not in dictionary:
+            dictionary[letter] = 1
+        else:
+            dictionary[letter] += 1
+    
+    for key in dictionary.keys():
+        print(key + str(dictionary[key]), end="")
+    return ""
+
+string_compression("AAAABBBBCCCCCDDEEEE")   # A4B4C5D2E4
