aabb7cf5e4aaeed0e823ecc65ba8ff8590a75932
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-23 18:47:56 -0500

    [added] rotation algorithm

--- algorithms/arrays/rotation.py
+++ algorithms/arrays/rotation.py
@@ -1,12 +1,12 @@
 # A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become . Note that the lowest index item moves to the highest index in a rotation. This is called a circular array
 
-def rotLeft(a, d):
+def rotation(a, positions):
     # Write your code here
     k = len(a)
     new_array = a.copy()
     
     for i in range(k):
-        new_index = i - d
+        new_index = i - positions
         
         if new_index < 0 :
             new_index = new_index + k 
@@ -14,3 +14,9 @@ def rotLeft(a, d):
         new_array[new_index] = a[i]
         
     return new_array
+
+array = [1, 2, 3, 4, 5, 6, 7, 8]
+positions = 1
+
+print(array)
+print(rotation(array, positions))
