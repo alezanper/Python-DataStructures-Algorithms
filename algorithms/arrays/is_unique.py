ad303f7638f56083ddae23fd5f0a6e9af27f8fb6
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-24 21:00:52 -0500

    [added] is unique

---
+++ algorithms/arrays/is_unique.py
@@ -0,0 +1,18 @@
+# Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures
+
+def is_unique(string):
+    d = {}
+    for i in range(len(string)):
+        if (string[i] in d):
+            d[string[i]] += 1
+        else:
+            d[string[i]] = 1
+    
+    for j in d.values():
+        if j > 1:
+            return False
+    return True
+
+print(is_unique("Hello"))
+print(is_unique("world"))
+
