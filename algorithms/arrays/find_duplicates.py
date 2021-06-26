d86959af5d5c762056e3af3d56dbc4d37fbe0740
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-26 17:50:28 -0500

    [added] find duplciates

---
+++ algorithms/arrays/find_duplicates.py
@@ -0,0 +1,16 @@
+# Given an array of n elements that contains elements from 0 to n-1, with any of these numbers appearing any number of times. Find these repeating numbers in O(n) and using only constant memory space.
+# Input : n = 7 and array[] = {1, 2, 3, 6, 3, 6, 1}
+# Output: 1, 3, 6
+
+def find_duplicates(arr):
+    seen = set()
+    duplicates = []
+    for i in range(len(arr)):
+        if arr[i] not in seen:
+            seen.add(arr[i])
+        else:
+            duplicates.append(arr[i])
+    
+    return duplicates
+
+print(find_duplicates([1,2,3,4,6,7,2,3]))
