abb258231a74e06acc300ba9121de5e1badcc2e7
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-26 12:50:08 -0500

    [updated] pair sum

--- algorithms/arrays/pair_sum.py
+++ algorithms/arrays/pair_sum.py
@@ -1,29 +1,18 @@
 # Given an integer array, output all the unique pairs that sum up to a specific value k
 # pair_sum([1,3,2,2], 4)  returns 2 (1,3) and (2,2)
 
-def pair_sum(array, sum):
-    pairs = 0
-    couplecreated = False
+def pair_sum(arr, sum):
+    
+    couples = 0
 
-    k = len(array)
+    while(len(arr) > 0):
+        for i in range(1, len(arr)):
+            if (arr[0] + arr[i] == sum):
+                couples += 1
+        
+        del(arr[0])
 
-    if(k <= 1):
-        return 0
-
-    while(k>1):
-        for i in range(1, k, 1):
-            if(array[0] + array[i] == sum):
-                couplecreated = True
-                del(array[0])
-                del(array[i-1])
-                break
-        if(couplecreated):
-            pairs += 1
-            k = len(array)
-            couplecreated = False
-        else:
-            del(array[0])
-    return pairs
+    return couples
 
 a = [1,3,2,2]
 sum = 4
