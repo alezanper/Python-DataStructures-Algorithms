20fa6e146476e7b2019e0844721fc08d2ca5c67c
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-25 21:08:21 -0500

    [added] palindrome permutation

---
+++ algorithms/arrays/palindrome_permutation.py
@@ -0,0 +1,32 @@
+# Given a string, write a function to check if it is a permutation of a palindrome.
+# example:
+# input: Tact Coa
+# output True (permutations: "taco cat", "atco cta")
+
+def palindrome_permutation(string):
+
+    string = string.lower()
+    dictionary = {}
+    ones = 0
+
+    for character in string:
+        if character == " ":
+            continue
+        if character in dictionary:
+            dictionary[character] += 1
+        else:
+            dictionary[character] = 1
+
+    for count in dictionary.values():
+        if count not in (1, 2):
+            return False
+        if count == 1:
+            ones += 1
+    
+    if ones > 1:
+        return False
+
+    return True        
+
+print(palindrome_permutation("Tact Coa"))
+print(palindrome_permutation("Tact Coas"))
