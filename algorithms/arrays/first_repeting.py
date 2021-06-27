b60242ee396851f768b9620a0d8b0d36c5198414
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-26 21:49:34 -0500

    [added] first repeting

---
+++ algorithms/arrays/first_repeting.py
@@ -0,0 +1,26 @@
+# Given an array of integers, find the first repeating element in it. We need to find the element that occurs more than once and whose index of first occurrence is smallest. 
+# Input:  arr[] = {10, 5, 3, 4, 3, 5, 6}
+# Output: 5 [5 is the first element that repeats]
+
+def first_repeting(arr):
+    dictionary = {}
+    repeted = []
+
+    for i in range(len(arr)):
+        if arr[i] in dictionary:
+            dictionary[arr[i]] +=1
+        else:
+            dictionary[arr[i]] = 1
+
+    for key, value in dictionary.items():
+        if (value > 1):
+            repeted.append(key)
+    
+    for i in range(len(arr)):
+        if (arr[i] in repeted):
+            return arr[i]
+    
+    return False
+
+print(first_repeting([10, 5, 3, 4, 3, 5, 6]))
+
---
+++ algorithms/arrays/first_repeting_2.py
@@ -0,0 +1,22 @@
+# Given an array of integers, find the first repeating element in it. We need to find the element that occurs more than once and whose index of first occurrence is smallest. 
+# Input:  arr[] = {10, 5, 3, 4, 3, 5, 6}
+# Output: 5 [5 is the first element that repeats]
+
+def first_repeting(arr):
+
+    seen = set()
+    repeted = set()
+
+    for i in range(len(arr)):
+        if arr[i] not in seen:
+            seen.add(arr[i])
+        else:
+            repeted.add(arr[i])
+    
+    for i in range(len(arr)):
+        if arr[i] in repeted:
+            return arr[i]
+
+    return 0
+    
+print(first_repeting([10, 5, 3, 4, 3, 5, 6]))
