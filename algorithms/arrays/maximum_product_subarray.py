07cd53aa5c3b226c79aa8b4255fbf78d3a989242
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-27 14:34:37 -0500

    [added] hard problem

---
+++ algorithms/arrays/maximum_product_subarray.py
@@ -0,0 +1,5 @@
+# Given an array that contains both positive and negative integers, find the product of the maximum 
+# product subarray. Expected Time complexity is O(n) and only O(1) extra space can be used.
+# Input: arr[] = {6, -3, -10, 0, 2}
+# Output:   180  // The subarray is {6, -3, -10}
+
