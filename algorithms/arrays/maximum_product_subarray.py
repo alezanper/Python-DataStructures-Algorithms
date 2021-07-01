5c634a2819dc1c69ce8a7a9f0ed965e2c4f252f1
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-30 21:58:55 -0500

    [added] maximun product subarray

--- algorithms/arrays/maximum_product_subarray.py
+++ algorithms/arrays/maximum_product_subarray.py
@@ -3,3 +3,13 @@
 # Input: arr[] = {6, -3, -10, 0, 2}
 # Output:   180  // The subarray is {6, -3, -10}
 
+def prods(arr):
+    product = current_product = arr[0] * arr[1] 
+
+    for num in arr[2:]:
+        current_product = max(current_product * num, num)
+        product = max(current_product, product)
+    return product
+
+
+print(prods([8,1,2,-3,4,5,-1])) # 20
