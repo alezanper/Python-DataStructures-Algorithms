70bdbcebf344906027398e4309368357f5fc1d32
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-30 19:56:05 -0500

    [added] big o

---
+++ data-structures/big_o.sh
@@ -0,0 +1,34 @@
+-----------
+---LISTS---
+-----------
+Operation	        Big-O Efficiency
+------------------  ----------------
+index []	        O(1)
+index assignment	O(1)
+append	            O(1)
+pop()	            O(1)
+pop(i)	            O(n)
+insert(i,item)	    O(n)
+del operator	    O(n)
+iteration	        O(n)
+contains (in)	    O(n)
+get slice [x:y]	    O(k)
+del slice	        O(n)
+set slice	        O(n+k)
+reverse	            O(n)
+concatenate	        O(k)
+sort	            O(n log n)
+multiply	        O(nk)
+
+----------------
+---DICTIONARY---
+----------------
+Operation	        Big-O Efficiency
+------------------  ----------------
+copy	            O(n)
+get item	        O(1)
+set item	        O(1)
+delete item	        O(1)
+contains (in)	    O(1)
+iteration	        O(n)
+
