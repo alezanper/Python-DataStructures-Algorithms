96f1e41cb69a2a5872bb74d93fc681812b939340
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-07-04 10:53:40 -0500

    [added] notes

--- algorithms/arrays/match_string.py
+++ algorithms/arrays/match_string.py
@@ -1,4 +1,5 @@
-# There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
+# There is a collection of input strings and a collection of query strings. For each query string, 
+# determine how many times it occurs in the list of input strings. Return an array of the results.
 # Example:
 # strings = ["hello", "world", "from", "python", "python"]
 # queries = ["hello", "alex", "python"]
